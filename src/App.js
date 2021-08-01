fd28584336692b0f8d6dd5e1b395b36250008451
Ivan Guzman <n3cr0.2012@gmail.com>
2021-07-31 23:14:32 -0700

    Added Tailwind CSS to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import "./styles/tailwind.css";
 
 export default function App() {
+  const [markdown, setMarkdown] = useState("# Start creating your markdown");
+
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
+
   return (
     <div className="app">
-      <textarea />
+      <textarea onChange={handleChange} value={markdown} />
 
-      <div className="preview" />
+      <ReactMarkdown className="preview" children={markdown} />
     </div>
   );
 }
