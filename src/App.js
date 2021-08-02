933d42c0d44d5998e466cb1eebce29b1246cd9c1
Ivan Guzman <n3cr0.2012@gmail.com>
2021-08-01 17:36:02 -0700

    Removed tailwind test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
-import "./styles/tailwind.css";
+import "./styles/App.css";
 
 export default function App() {
-  const [markdown, setMarkdown] = useState("# Start creating your markdown");
-
+  const [markdown, setMarkdown] = useState("# Lets get started with Markdown");
   function handleChange(e) {
     setMarkdown(e.target.value);
   }
-
   return (
     <div className="app">
       <textarea onChange={handleChange} value={markdown} />
